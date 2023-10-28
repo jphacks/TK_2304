6303e5f7ed3cca08de1ccefd51b1c0e466242230
pentabi <tabitosatoh@gmail.com>
2023-10-28 17:35:57 +0900

    Got chatGPT to work

--- camembert-chat/src/App.js
+++ camembert-chat/src/App.js
@@ -1,17 +1,14 @@
-import './App.css';
-import SignIn from './components/SignIn';
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth} from "./firebase"
-import Chat from "./components/Chat"
+import "./App.css";
+import SignIn from "./components/SignIn";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Chat from "./components/Chat";
+
 
 function App() {
   const [user, setUser] = useAuthState(auth);
 
-  return (
-    <div>
-      {user ? <Chat></Chat> : <SignIn></SignIn>}
-    </div>
-  );
+  return <div>{user ? <Chat></Chat> : <SignIn></SignIn>}</div>;
 }
 
 export default App;
