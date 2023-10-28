72254d750efc4b6944a19b666e007ab730c53027
pentabi <tabitosatoh@gmail.com>
2023-10-28 11:43:20 +0900

    displayed the translation(not actually translated)

--- camembert-chat/src/components/Chat.js
+++ camembert-chat/src/components/Chat.js
@@ -1,31 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import SingOut from "./SignOut";
 import { db, auth } from "../firebase";
-import SendMessage from './SendMessage';
+import SendMessage from "./SendMessage";
 
 function Chat() {
   const [messages, setMessages] = useState([]);
   useEffect(() => {
-    db.collection("messages").orderBy("createdAt").limit(50).onSnapshot((snapshot) => {
-      setMessages(snapshot.docs.map((doc) => doc.data()))
-    })
-  }, [])
+    db.collection("messages")
+      .orderBy("createdAt")
+      .limit(50)
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  }, []);
 
   return (
     <div>
       {console.log(messages)}
       <SingOut></SingOut>
-      <div className='msgs'>
-        {messages.map(({ id, text, photoURL, uid }) => (
-          <div key={id} className={`msg ${uid === auth.currentUser.uid ? "sent" : "recieved"}`}>
-            <img src={photoURL} alt=''></img>
+      <div className="msgs">
+        {messages.map(({ id, text, photoURL, uid, translation }) => (
+          <div
+            key={id}
+            className={`msg ${
+              uid === auth.currentUser.uid ? "sent" : "recieved"
+            }`}
+          >
+            <img src={photoURL} alt=""></img>
             <p>{text}</p>
+            {/* translation text */}
+            {translation && (
+              <p className="translation">Translation: {translation}</p>
+            )}
           </div>
         ))}
       </div>
       <SendMessage></SendMessage>
     </div>
-  )
+  );
 }
 
 export default Chat;
