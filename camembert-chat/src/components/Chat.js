740cb23efa231ff238e46f5f8b33d5536ba5d03f
pentabi <tabitosatoh@gmail.com>
2023-10-28 17:57:30 +0900

    ittann merge

--- camembert-chat/src/components/Chat.js
+++ camembert-chat/src/components/Chat.js
@@ -35,6 +35,7 @@ function Chat() {
           </div>
         ))}
       </div>
+      {/* <GetMessage></GetMessage> */}
       <SendMessage></SendMessage>
     </div>
   );
--- camembert-chat/src/index.js
+++ camembert-chat/src/index.js
@@ -6,7 +6,7 @@ import ChatGpt from "./ChatGpt";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <ChatGpt />
+    <App />
+    {/* <ChatGpt /> */}
   </React.StrictMode>
 );
